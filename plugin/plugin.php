1493a3017dbbddc56045f5ffab0875bc18a32594
Hichem Chabou <41421720+Hichem-Chabou@users.noreply.github.com>
2020-10-19 11:55:42 +0100

    Initial upload

---
+++ plugin/plugin.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  Plugin Name:
+  Plugin URI:
+  Description:
+  Version:
+  Author: 
+  Author URI:
+  License: GPLv2 or later
+ */
+
+// exit if file is called directly
+if ( ! defined( 'ABSPATH' ) ) {
+
+	exit;
+
+}
+
+
+
+
+
+
+
+
+
